1d20a6924d6cda7f95429dfa5b57e696666dd1c4
Code Hz <CodeHz@outlook.com>
2019-02-10 17:23:55 +0800

    add cli
    Signed-off-by: Code Hz <CodeHz@outlook.com>

---
+++ cli.js
@@ -0,0 +1,21 @@
+#!/bin/env node
+const api = require("./index");
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log("init", api.init());
+console.log("attach", api.attach());
+
+rl.prompt();
+
+rl.on("line", async line => {
+  console.log((await api.command.execute("cli", line)).trim());
+  rl.prompt();
+}).on('close', () => {
+  console.log('Have a great day!');
+  process.exit(0);
+});
